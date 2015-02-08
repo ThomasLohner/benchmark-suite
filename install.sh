2fe2a9a38a5468aff4129e9d9447f2fecedbd9d0
Thomas Lohner <t.lohner@syseleven.de>
2015-02-08 18:24:11 +0100

    add ioping

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ DISTR=$(grep -h '^NAME=' /etc/*release | sed -e 's/NAME=//g' | tr -d '[:punct:]'
 case $DISTR in
      UBUNTU)
           apt-get update
-          apt-get install puppet-common git pwgen python-software-properties cowsay siege -y
+          apt-get install puppet-common git pwgen python-software-properties cowsay siege ioping -y
 	  APACHEUSER="www-data"
           ;;
      GENTOO)
@@ -13,7 +13,7 @@ case $DISTR in
           sed -i '/dev-db\/mariadb/d' /etc/portage/package.use
           sed -i '/virtual\/mysql/d' /etc/portage/package.use
           emerge --sync
-          emerge -n app-admin/puppet dev-vcs/git app-admin/pwgen games-misc/cowsay app-benchmarks/siege
+          emerge -n app-admin/puppet dev-vcs/git app-admin/pwgen games-misc/cowsay app-benchmarks/siege app-benchmarks/ioping
           APACHEUSER="apache"
           ;;
      *)
